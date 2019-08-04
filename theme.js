f6ac1d3ef3e820bba26b14ec481895c539b02812
Ludya <52601945+Ludya@users.noreply.github.com>
2019-08-04 13:16:26 +0900

    2.00 ver

--- theme.js
+++ theme.js
@@ -1,26 +1,40 @@
+var link = {
+    setcolor:function(color){
+    // var arin = document.querySelectorAll('a');
+    //     var i = 0;
+    //     while(i < arin.length){
+    //     arin[i].style.color = color;
+    //     i = i + 1;
+    //     }
+    $('a').css('color',color);
+    }
+}
+var body={
+    setcolor:function(color){
+    // document.querySelector('body').style.color = color;
+    $('body').css('color',color);
+    },
+    setbackgroundcolor:function(color){
+    // document.querySelector('body').style.backgroundColor = color;
+    $('body').css('backgroundColor',color);
+    }
+}
+function ca(self){
+    var target = document.querySelector('body');
+    if(self.value === 'black'){
+        body.setbackgroundcolor('rgba(0, 0, 0, 0.877)');
+        body.setcolor('white')
+        self.value = 'white';
 
-            function ca(self){
-                var target = document.querySelector('body');
-                if(self.value === 'black'){
-                    target.style.backgroundColor = 'rgba(0, 0, 0, 0.877)';
-                    target.style.color = 'white';
-                    self.value = 'white'
-                    var arin = document.querySelectorAll('a');
-                    var i = 0;
-                    while(i < arin.length){
-                    arin[i].style.color = 'rgb(218, 89, 179)';
-                    i = i + 1;
-                    }
-                }
-                else{
-                    target.style.backgroundColor = 'white';
-                    target.style.color = 'black';
-                    self.value = 'black'
-                    var arin = document.querySelectorAll('a');
-                    var i = 0;
-                    while(i < arin.length){
-                    arin[i].style.color = 'black';
-                    i = i + 1;
-                    }
-                }
-            }
+        link.setcolor('rgb(218, 89, 179)');
+    }
+    else{
+        body.setbackgroundcolor('white');
+        body.setcolor('black');
+        self.value = 'black';
+        
+        link.setcolor('blue');
+    }
+}
+
+
